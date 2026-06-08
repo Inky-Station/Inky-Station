155b9339cbc337d146483c8d11ed0a766cfb991d
LuciferEOS <stepanteliatnik2022@gmail.com>
2026-06-09 00:50:13 +0200

    final

--- Content.Inky.Common/Whale/WhaleEvents.cs
+++ Content.Inky.Common/Whale/WhaleEvents.cs
@@ -6,3 +6,18 @@ public sealed class LeviathanMusicStartEvent : EntityEventArgs;
 
 [Serializable, NetSerializable]
 public sealed class LeviathanMusicStopEvent : EntityEventArgs;
+
+
+public sealed class GrappleEmbedCompletedEvent : EntityEventArgs // hate.
+{
+    public readonly EntityUid Embedded;
+    public readonly EntityUid Weapon;
+    public readonly EntityUid? Shooter;
+
+    public GrappleEmbedCompletedEvent(EntityUid embedded, EntityUid weapon, EntityUid? shooter)
+    {
+        Embedded = embedded;
+        Weapon = weapon;
+        Shooter = shooter;
+    }
+}
