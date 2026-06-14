f90535c09a2c4bfeb9728c890b47ea1d0efb530e
LuciferEOS <stepanteliatnik2022@gmail.com>
2026-06-15 00:57:41 +0200

    test

--- Content.Packaging/ServerPackaging.cs
+++ Content.Packaging/ServerPackaging.cs
@@ -104,6 +104,7 @@ public static class ServerPackaging
                 {
                     "build",
                     Path.Combine("Content.Trauma.Server", "Content.Trauma.Server.csproj"), // Trauma - Trauma.Server depends on everything
+                    Path.Combine("Content.Inky.Server", "Content.Inky.Server.csproj"), // inky - OH MY GODDDDDDDDDD
                     "-c", configuration,
                     "--nologo",
                     "/v:m",
