2303431b8e052fc2e59ed0ebe5b6ea7ec10072fa
Ted Lukin <fedorlukin2006@gmail.com>
2026-06-19 03:16:33 +0700

    adminwho for everyone

--- Content.Server/Administration/Commands/AdminWhoCommand.cs
+++ Content.Server/Administration/Commands/AdminWhoCommand.cs
@@ -7,7 +7,7 @@ using Robust.Shared.Utility;
 
 namespace Content.Server.Administration.Commands;
 
-[AdminCommand(AdminFlags.AdminWho)]
+// [AdminCommand(AdminFlags.AdminWho)] // inky - nah
 public sealed partial class AdminWhoCommand : LocalizedCommands
 {
     [Dependency] private IAfkManager _afkManager = default!;
